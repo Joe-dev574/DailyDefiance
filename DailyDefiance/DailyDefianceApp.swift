86c22fa7503bd706384ce8af19271c3a147b0928
JDeWeese <jdeweese574@icloud.com>
2024-08-18 10:12:52 -0500

    Initial Commit

---
+++ DailyDefiance/DailyDefianceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDefianceApp.swift
+//  DailyDefiance
+//
+//  Created by Joseph DeWeese on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyDefianceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
